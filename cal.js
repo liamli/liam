314f71b553d6208670e2739ac3ea2064913cde04
liamli <liam.li@live.com>
2017-08-11 16:59:07 +0800

    Done

--- cal.js
+++ cal.js
@@ -5,13 +5,12 @@ console.log("123");// from 为当前页，to 为下一页
 
 
 
-$("#cal3p1").bind("click", function(event) { 
-
+/* 3+1 产品 */
+function cal3p1(){
 
 	var p3p1_price = $("#3p1_price").val() ;
 	var p3p1_shoufu = $("#3p1_shoufu").val() ;
 
-	console.log( p3p1_price );
 
 	if ( !(p3p1_price>0) ) {
 		console.log( "未输入交易价格" );
@@ -31,4 +30,52 @@ $("#cal3p1").bind("click", function(event) {
 	$(".3p1_four").text( Math.round( p3p1_four ) );
 	$(".3p1_last").text( Math.round( p3p1_last ) );
 
+
+	$("#3y_price").val( p3p1_price );
+	$("#3y_shoufu").val( p3p1_shoufu );
+	$("#1px_price").val( p3p1_price );
+	$("#1px_shoufu").val( p3p1_shoufu );
+}
+
+
+
+
+/* 三年期产品 */
+function cal3y(){
+
+	var p3y_price = $("#3y_price").val() ;
+	var p3y_shoufu = $("#3y_shoufu").val() ;
+
+	if ( !(p3y_price>0) ) {
+		console.log( "未输入交易价格" );
+		return;
+	}
+
+	if ( !(p3y_shoufu>0) ) {
+		console.log( "未输入首付金额" );
+		return;
+	}
+
+	var p3y_one = (p3y_price - p3y_shoufu) * 0.0347 ;
+
+	$(".3y_one").text( Math.round( p3y_one ) );
+
+
+	$("#3p1_price").val( p3y_price );
+	$("#3p1_shoufu").val( p3y_shoufu );
+	$("#1px_price").val( p3y_price );
+	$("#1px_shoufu").val( p3y_shoufu );
+
+}
+
+
+$("#cal3p1").bind("click", function(event) { 
+	cal3p1();
+	cal3y();
+});
+
+
+$("#cal3y").bind("click", function(event) {
+	cal3p1();
+	cal3y();
 });
